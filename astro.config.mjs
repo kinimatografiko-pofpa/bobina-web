e04ea8637558996ca1234c8f46ae386d9b7f3c76
Manolis Skouris <skourismanolis@gmail.com>
2022-04-24 02:03:42 +0300

    add turbolinks for even better speed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import astroImagePlugin from 'astro-imagetools/plugin';
 
+import turbolinks from "@astrojs/turbolinks";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
-	vite: {
-		plugins: [astroImagePlugin],
-	},
+  integrations: [preact(), turbolinks()],
+  vite: {
+    plugins: [astroImagePlugin]
+  }
 });
