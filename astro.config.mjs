ebb63eae15b967e356d4a2e1d574cc45a7c4ddb0
Manolis Skouris <skourismanolis@gmail.com>
2022-04-23 18:50:13 +0300

    add astro-imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import astroImagePlugin from 'astro-imagetools/plugin';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [preact()],
+	vite: {
+		plugins: [astroImagePlugin],
+	},
 });
