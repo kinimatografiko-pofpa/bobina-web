82a4d720f355a5533d8e37961cd9bd2973ea5d59
Manolis Skouris <skourismanolis@gmail.com>
2022-07-08 13:11:46 +0300

    run yarn upgrade, remove deprecated turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import astroImagePlugin from 'astro-imagetools/plugin';
 
-import turbolinks from "@astrojs/turbolinks";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), turbolinks()],
-  vite: {
-    plugins: [astroImagePlugin]
-  }
+	integrations: [preact()],
+	vite: {
+		plugins: [astroImagePlugin],
+	},
 });
