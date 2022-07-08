cf9cfdf5680058a792d9e06cacec48c8ef5eca4c
Manolis Skouris <skourismanolis@gmail.com>
2022-07-08 16:27:54 +0300

    remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 import astroImagePlugin from 'astro-imagetools/plugin';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact(), tailwind()],
+	integrations: [tailwind()],
 	vite: {
 		plugins: [astroImagePlugin],
 	},
