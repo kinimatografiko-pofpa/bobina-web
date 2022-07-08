85236bdfeb9e5a6eb2df824acebb6984da0110da
Manolis Skouris <skourismanolis@gmail.com>
2022-07-08 16:58:21 +0300

    made blog header..

---
+++ src/wordpress/index.js
@@ -0,0 +1,27 @@
+const siteUrl =
+	'https://public-api.wordpress.com/rest/v1.1/sites/mpompina.wordpress.com';
+
+async function getPosts(max = 20, page_handle = '', category = '') {
+	let resp = await fetch(
+		siteUrl +
+			`/posts?number=${max}&page_handle=${page_handle}&category=${category}`
+	);
+
+	let data = await resp.json();
+
+	return { data, resp };
+}
+
+// async function getAllPosts() {
+// 	let allPosts = [];
+
+// 	let { data } = await getPosts(100);
+// 	allPosts.push(data);
+// 	if (data.found > 100) {
+// 		while (data)
+// 	}
+// }
+
+export default {
+	getPosts,
+};
