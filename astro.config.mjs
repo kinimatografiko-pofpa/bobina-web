7875fb3f0ba12a9fb57a7a0c0aa99b91d50e8ffb
Manolis Skouris <skourismanolis@gmail.com>
2022-07-08 13:13:30 +0300

    add tailwind dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import astroImagePlugin from 'astro-imagetools/plugin';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
-	vite: {
-		plugins: [astroImagePlugin],
-	},
+  integrations: [preact(), tailwind()],
+  vite: {
+    plugins: [astroImagePlugin]
+  }
 });
