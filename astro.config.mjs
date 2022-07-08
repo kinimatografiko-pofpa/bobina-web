1102d5845fc972999e881d55c8a6b01c0245db11
Manolis Skouris <skourismanolis@gmail.com>
2022-07-08 15:35:24 +0300

    add prettier and base carousel component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import astroImagePlugin from 'astro-imagetools/plugin';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
-  vite: {
-    plugins: [astroImagePlugin]
-  }
+	integrations: [preact(), tailwind()],
+	vite: {
+		plugins: [astroImagePlugin],
+	},
 });
