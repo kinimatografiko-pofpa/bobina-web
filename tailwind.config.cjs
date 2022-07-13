f416589c91f75ade8fcbe8ecf90642ad6ba72190
Manolis Skouris <skourismanolis@gmail.com>
2022-07-13 21:15:45 +0300

    add navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				black: '#131313',
+			},
+		},
 	},
 	plugins: [],
 };
