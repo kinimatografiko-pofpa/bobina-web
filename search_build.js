29418d3dae280c386bdf1fdbca290773fcdafaad
Manolis Skouris <skourismanolis@gmail.com>
2022-08-24 14:52:13 +0300

    fix search but it's pretty weird

--- search_build.js
+++ search_build.js
@@ -1,6 +1,46 @@
 const axios = require('axios');
 const { parse } = require('node-html-parser');
 
+/**
+ * returns the post excerpt with the author removed
+ * @param {Object} post
+ * @param {Number} [max_chars] max char count excl ending of excerpt. Will be cut to the nearest word.
+ * @returns {String} excerpt
+ */
+function getExcerptFromPost(post, max_chars) {
+	const EXCERPT_END = '[…]';
+	let parsedPost = parse(post.content);
+	let paragraphElems = parsedPost.querySelectorAll('p');
+	let authorText;
+
+	//iterate over paragraph elements to find the first with text, usually the first contains the author text
+	for (const el of paragraphElems) {
+		if (el.text.length > 0) {
+			authorText = el.text;
+			break;
+		}
+	}
+
+	let exc = parse(post.excerpt).text;
+	exc = exc.replace(authorText, '');
+
+	if (max_chars != null && !isNaN(max_chars) && max_chars >= 0) {
+		let words = exc.split(' ');
+		let total = 0;
+		exc = ''; //empty for reconstruction
+		for (const word of words) {
+			//add +1 because of space
+			if (total + word.length + 1 <= max_chars) {
+				total += word.length + 1;
+				exc += word + ' ';
+			}
+		}
+		exc += EXCERPT_END;
+	}
+
+	return exc;
+}
+
 const siteUrl =
 	'https://public-api.wordpress.com/rest/v1.1/sites/mpompina.wordpress.com';
 
@@ -33,9 +73,12 @@ async function main() {
 			objectID: p.ID,
 			title: p.title,
 			content: parse(p.content).text,
-			imageUrl: getRecordImage(p),
-			categories: Object.keys(p.categories),
+			image: getRecordImage(p),
+			categories: p.categories,
+			shortText: getExcerptFromPost(p),
 			tags: Object.keys(p.tags),
+			date: p.date,
+			slug: p.slug,
 		}));
 	} catch (error) {
 		console.log(error);
