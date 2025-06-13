cfebf21fba50df408e275688a00072e7995291db
VangelisDimi <evangelos.dhmh@gmail.com>
2025-06-13 18:02:00 +0300

    styling changes and reccomendation tweaking

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,32 +11,8 @@ const posts = defineCollection({
             id: p.slug,
             ...p,
             featured_image: wordpress.getFeaturedImage(p),
-            // ID: p.ID,
-            // title: p.title,
-            // slug: p.slug,
-            // date: p.date,
-            // content: p.content,
-            // tags: p.tags,
-            // attachments: p.attachments,
-            // attachment_count: p.attachment_count,
         }));
     },
-    // schema: z.object({
-    //     id: z.string(),
-    //     ID: z.number(),
-    //     title: z.string(),
-    //     slug: z.string(),
-    //     date: z.string(),
-    //     content: z.string(),
-    //     tags: z.record(z.string(),z.object({
-    //         name:z.string(),
-    //         slug:z.string(),
-    //     })),
-    //     attachments: z.record(z.string() ,z.object({
-    //         URL: z.string(),
-    //         mime_type: z.string(),
-    //     })),
-    // }),
 });
 
 const categories = defineCollection({
@@ -52,13 +28,14 @@ const categories = defineCollection({
             if (c.ID == wordpress.DEFAULT_CATEGORY) {
                 return {
                     id: c.slug,
+                    ...c,
                     name: 'Όλα τα άρθρα',
                     href: '/posts',
                 };
             }
             return {
                 id: c.slug,
-                name: c.name,
+                ...c,
                 href: '/categories/' + c.slug,
             };
         });
